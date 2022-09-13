1fe04f7edbca057c8c61527f48e440637317da6d
relero90 <relero90@gmail.com>
2022-09-13 15:49:55 -0600

    required sequelize in server.js for sequelize.sync()

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
+const sequelize = require("./config/connection");
+const chalk = require("chalk");
 // import sequelize connection
 
 const app = express();
@@ -12,5 +14,7 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("App listening on port ${PORT}!🧞‍♀️"));
+  app.listen(PORT, () =>
+    console.log(chalk.magenta(`App listening at http://localhost:${PORT}!`))
+  );
 });
